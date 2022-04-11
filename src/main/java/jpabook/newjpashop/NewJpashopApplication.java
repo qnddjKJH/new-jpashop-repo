991d50f3b14a84eb959332109bc941d4346e90ea
qnddjkjh.dev <qnddjkjh.dev@gmail.com>
2022-04-11 20:50:22 +0900

    init project

---
+++ src/main/java/jpabook/newjpashop/NewJpashopApplication.java
@@ -0,0 +1,13 @@
+package jpabook.newjpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewJpashopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewJpashopApplication.class, args);
+    }
+
+}
