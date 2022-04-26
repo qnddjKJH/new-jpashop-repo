d90764fbe329de3b3287f0f85089a31b9ba79c2f
qnddjkjh.dev <qnddjkjh.dev@gmail.com>
2022-04-26 20:27:56 +0900

    추가 주문조회 api v1
     - 안좋은 예
      - 엔티티 직접 노출

---
+++ src/main/java/jpabook/newjpashop/api/OrderApiController.java
@@ -0,0 +1,32 @@
+package jpabook.newjpashop.api;
+
+import jpabook.newjpashop.domain.Order;
+import jpabook.newjpashop.domain.OrderItem;
+import jpabook.newjpashop.repository.OrderRepository;
+import jpabook.newjpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getUsername();
+            order.getDelivery().getAddress();
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+
+}
