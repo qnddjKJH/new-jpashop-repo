acd65df6e0e6ad010c5f6f524ee9bbc1d26ad48c
qnddjkjh.dev <qnddjkjh.dev@gmail.com>
2022-04-26 20:22:28 +0900

    수정 빠진 내용
     - 양방향 관계에서 @JsonIgnore 생략해서 무한 루프 걸림

--- src/main/java/jpabook/newjpashop/domain/Delivery.java
+++ src/main/java/jpabook/newjpashop/domain/Delivery.java
@@ -1,5 +1,6 @@
 package jpabook.newjpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
@@ -15,6 +16,9 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
+    // 한쪽을 안 막아두면 무한하게 서로를 부름
+    // 양방향 관계라면 주인이 아닌 한쪽은 막아둬야 한다.
+    @JsonIgnore 
     @OneToOne(mappedBy = "delivery")
     private Order order;
 
--- src/main/java/jpabook/newjpashop/domain/OrderItem.java
+++ src/main/java/jpabook/newjpashop/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package jpabook.newjpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jpabook.newjpashop.domain.item.Item;
 import lombok.*;
 
@@ -16,11 +17,12 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
